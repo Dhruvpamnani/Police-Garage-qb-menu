fd24f525332204c79a8e51b1e8b781233f6a0fdb
Dhruvpamnani <90241955+Dhruvpamnani@users.noreply.github.com>
2021-12-28 00:17:06 +0530

    Update Config.lua

--- Dox-Garage/Config.lua
+++ Dox-Garage/Config.lua
@@ -4,13 +4,3 @@ Config.Pedlocation = {
     {Cords = vector3(441.41162, -974.6804, 24.699926), h = 172.08871}  -- NPC Spawn Location.
 }
 
-
--- Dox Shop https://discord.gg/wQBuB3U5Ym
--- Dox Shop https://discord.gg/wQBuB3U5Ym
--- Dox Shop https://discord.gg/wQBuB3U5Ym
--- Dox Shop https://discord.gg/wQBuB3U5Ym
--- Dox Shop https://discord.gg/wQBuB3U5Ym
--- Dox Shop https://discord.gg/wQBuB3U5Ym
--- Dox Shop https://discord.gg/wQBuB3U5Ym
--- Dox Shop https://discord.gg/wQBuB3U5Ym
-
